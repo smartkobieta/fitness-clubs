60199b57556d0ad71572d36b3326bbd2532a0f23
smartkobieta <allavovk39@gmail.com>
2023-08-16 15:00:06 +0200

    Update script-1m.js

--- js/script-1m.js
+++ js/script-1m.js
@@ -1,5 +1,30 @@
-const age = 10;
+//1. Округление цифр в рандоме
 
-console.log("цена", 5);
-console.log("dvdsvsd 5 dvd");
-console.log(15);
+//const max = 80;
+//const min = 10;
+
+//console.log(Math.random() * (max - min) + min);
+
+//console.log(Math.round(Math.random() * (max - min) + min));
+
+// 2. Вибір рандомом кольору
+
+//const colors = ["tomato", "teal", "orange", "deeppink", "skyblue"];
+//const max = colors.length - 1;
+//const min = 0;
+//const index = Math.round(Math.random() * (max - min) + min);
+//const color = colors[index];
+//console.log(color);
+//document.body.style.backgroundColor = color;
+
+//3.
+const numbers = [51, 18, 13, 24, 7, 85, 9];
+let biggestNumber = number[0];
+
+for (const number of numbers) {
+  if (number > biggestNumber) {
+    biggestNumber = number;
+  }
+}
+
+console.log("biggestNumber: ", biggestNumber);
